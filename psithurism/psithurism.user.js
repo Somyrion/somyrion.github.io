2bcb063ea7fd6b7d0b1fffa2211339b609ef19a5
dithpri <dithpri@gmail.com>
2020-09-27 15:48:43 +0200

    Fix K targetting

--- psithurism/psithurism.user.js
+++ psithurism/psithurism.user.js
@@ -27,8 +27,12 @@
 */
 
 /* global $ */
+$.fn.random = function() {
+  return this.eq(Math.floor(Math.random() * this.length));
+}
 
-var facID = "21"; // update when N-Day starts!
+const facID = "21"; // update when N-Day starts!
+
 (function() {
 	var shifted = false;
 	var controlled = false;
@@ -135,15 +139,15 @@ var facID = "21"; // update when N-Day starts!
 				if (window.location.href.indexOf("page=faction") > -1 && window.location.href.indexOf("fid="+facID) <= -1 && window.location.href.indexOf("view=nations") <= -1) {
 					$('a.nukestat-nations')[0].click();
 				}
-				// if on the faction's list of nations, choose the first non-fully-irradiated nation
+				// if on the faction's list of nations, choose a random non-fully-irradiated nation
 				else if (window.location.href.indexOf("page=faction") > -1 && window.location.href.indexOf("fid="+facID) <= -1 && window.location.href.indexOf("view=nations") > -1) {
-					var linkToTarget = $('ol li:not(:has(.nukedestroyedicon)) a')[0].href;
+					var linkToTarget = $('ol li:not(:has(.nukedestroyedicon)) a').random()[0].href;
 					var regexFindNation = /(?<=nation=).*(?=\/page=nukes)/g;
 					var nationToTarget = linkToTarget.match(regexFindNation)[0];
 					window.location.href = "https://www.nationstates.net/nation="+nationToTarget+"/page=nukes?target="+nationToTarget;
 				}
 				// if on the targetting page, calculate the appropriate number of nukes to target
-				else if (window.location.href.indexOf("/nation=") > -1 && window.location.href.indexOf("page=nukes") > -1) {
+				else if (window.location.href.indexOf("?target=") > -1 && window.location.href.indexOf("page=nukes") > -1) {
 					var regexFindNumber = /\d+/g;
 					var alreadyTargeted = parseInt($('.nukestat-targeted').text().match(regexFindNumber)[0]);
 					var alreadyRads = parseInt($('.nukestat-radiation').text().match(regexFindNumber)[0]);
